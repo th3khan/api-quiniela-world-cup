59bd1fb19a70cce018608b045fc6460e3178f44d
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-27 18:06:47 -0400

    validate if will be a seeder apply in run time

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"reflect"
 	"strconv"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"github.com/th3khan/api-quiniela-world-cup/pkg/seeders"
 	"github.com/th3khan/api-quiniela-world-cup/pkg/utils"
 	"github.com/th3khan/api-quiniela-world-cup/platform/migrations"
 )
@@ -20,6 +24,28 @@ func main() {
 
 	if !fiber.IsChild() {
 		migrations.Migrate()
+	}
+
+	args := os.Args
+
+	seeder := seeders.NewSeeder()
+	if len(args) > 0 {
+		otherArgs := args[1:]
+		for _, argName := range otherArgs {
+			argList := strings.Split(argName, "=")
+			if len(argList) == 2 {
+				if argList[0] == "--seed" {
+					fmt.Println(argList)
+					action := argList[1]
+					valid := reflect.ValueOf(seeder).MethodByName(action).IsValid()
+					if valid {
+						reflect.ValueOf(seeder).MethodByName(action).Call([]reflect.Value{})
+					} else {
+						log.Fatal(fmt.Sprintf("Seeder: %s, not found", action))
+					}
+				}
+			}
+		}
 	}
 
 	port, _ := strconv.Atoi(os.Getenv("APP_PORT"))
