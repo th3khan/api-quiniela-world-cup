605e18d2c4a93f2e163111755e96e072858aa74c
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-27 20:37:31 -0400

    user seeder for create user super admin

---
+++ pkg/seeders/users.go
@@ -0,0 +1,42 @@
+package seeders
+
+import (
+	"log"
+
+	"github.com/th3khan/api-quiniela-world-cup/app/models"
+	"github.com/th3khan/api-quiniela-world-cup/app/services/user"
+	"github.com/th3khan/api-quiniela-world-cup/pkg/utils"
+)
+
+func (s *Seed) CreateUsers() error {
+	passwordDefault, err := utils.HashingPassword("123456")
+	if err != nil {
+		return err
+	}
+	users := []models.User{
+		models.User{
+			ID:            1,
+			RoleId:        models.ROLE_SUPER_ADMIN,
+			Name:          "Super admin",
+			Email:         "superadmin@mail.com",
+			Password:      passwordDefault,
+			Active:        true,
+			EmailVerified: true,
+		},
+	}
+
+	for _, u := range users {
+		if err, _ := user.CreateUser(
+			u.Name,
+			u.Email,
+			u.RoleId,
+			u.Password,
+			u.Active,
+		); err != nil {
+			log.Fatal(err)
+			return err
+		}
+	}
+
+	return nil
+}
