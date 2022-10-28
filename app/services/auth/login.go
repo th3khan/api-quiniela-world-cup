143271d4f249b5520743352d08608945646bae28
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-28 01:47:59 -0400

    login service

---
+++ app/services/auth/login.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber/v2"
+	"github.com/th3khan/api-quiniela-world-cup/app/helpers"
+	"github.com/th3khan/api-quiniela-world-cup/app/models"
+	"github.com/th3khan/api-quiniela-world-cup/app/repositories"
+	"github.com/th3khan/api-quiniela-world-cup/platform/database"
+)
+
+func Login(email string, password string) (error, string, *models.User) {
+	db := database.Connection()
+	repo := repositories.NewUserRespository(db)
+
+	user := repo.GetUserByEmail(email)
+
+	if user.ID == 0 {
+		return fiber.NewError(fiber.StatusBadRequest, "Usuario no encontrado."), "", &models.User{}
+	}
+
+	if !helpers.CheckPasswordHash(password, user.Password) {
+		return fiber.NewError(fiber.StatusUnauthorized, "Credenciales invalidas"), "", &models.User{}
+	}
+
+	token, err := createToken(&user)
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error()), "", &models.User{}
+	}
+
+	return nil, token, &user
+}
+
+func createToken(user *models.User) (string, error) {
+	claims := jwt.MapClaims{
+		"name":       user.Name,
+		"email":      user.Email,
+		"roleId":     user.RoleId,
+		"authorized": true,
+		"exp":        time.Now().Add(time.Hour * 2).Unix(),
+	}
+
+	return helpers.GenerateToken(claims)
+
+}
