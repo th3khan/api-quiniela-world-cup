5fcf6a1bdd6f40f6e44f13aad95f176669137502
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-28 01:48:50 -0400

    create login user

--- app/controllers/auth/login.go
+++ app/controllers/auth/login.go
@@ -1,12 +1,37 @@
 package auth
 
-import "github.com/gofiber/fiber/v2"
-
-type LoginResponse struct {
-	User  string
-	Token string
-}
+import (
+	"github.com/go-playground/validator"
+	"github.com/gofiber/fiber/v2"
+	"github.com/th3khan/api-quiniela-world-cup/app/services/auth"
+	"github.com/th3khan/api-quiniela-world-cup/pkg/entities"
+)
 
 func Login(ctx *fiber.Ctx) error {
-	return ctx.JSON(LoginResponse{User: "user", Token: "53265323uvdjkvsaduaysd"})
+	request := entities.LoginRequest{}
+
+	if err := ctx.BodyParser(&request); err != nil {
+		return err
+	}
+
+	validate := validator.New()
+	err := validate.Struct(&request)
+
+	if err != nil {
+		return err
+	}
+
+	err, token, user := auth.Login(request.Email, request.Password)
+
+	if err != nil {
+		return err
+	}
+
+	var response entities.LoginResponse
+
+	response.User = entities.CreateUserResponse(user)
+	response.AccessToken = token
+
+	return ctx.JSON(response)
+
 }
