5d8ee7542e93e391665ad4c4a52a91331da0a02a
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-27 20:38:02 -0400

    add const ROLE_SUPER_ADMIN in role model

--- app/models/role.go
+++ app/models/role.go
@@ -3,8 +3,10 @@ package models
 import "time"
 
 type Role struct {
-	ID        uint      `gorm:"promaryKey"`
-	Name      string    `gorm:"uniqueIndex"`
+	ID        uint      `gorm:"primaryKey"`
+	Name      string    `gorm:"uniqueIndex;notNull"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"`
 }
+
+const ROLE_SUPER_ADMIN = 1
