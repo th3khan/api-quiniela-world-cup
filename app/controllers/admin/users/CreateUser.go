3474c0fc58363d756fad232658618659ad5af72a
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-30 09:55:41 -0400

    create user with image and email verified values

--- app/controllers/admin/users/CreateUser.go
+++ app/controllers/admin/users/CreateUser.go
@@ -1,9 +1,15 @@
 package users
 
 import (
+	"image"
+
 	"github.com/go-playground/validator"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"github.com/th3khan/api-quiniela-world-cup/app/helpers"
+	"github.com/th3khan/api-quiniela-world-cup/app/models"
 	"github.com/th3khan/api-quiniela-world-cup/app/repositories"
+	"github.com/th3khan/api-quiniela-world-cup/pkg/entities"
 	"github.com/th3khan/api-quiniela-world-cup/pkg/entities/admin"
 	"github.com/th3khan/api-quiniela-world-cup/platform/database"
 )
@@ -22,6 +28,18 @@ func CreateUser(ctx *fiber.Ctx) error {
 		return err
 	}
 
+	var image image.Image
+	var imageType string
+	imageIsDefinedAndValid := false
+
+	if len(request.Image) > 0 {
+		image, imageType, err = helpers.ValidateImage(request.Image)
+		if err != nil {
+			return fiber.NewError(fiber.StatusBadRequest, err.Error())
+		}
+		imageIsDefinedAndValid = true
+	}
+
 	db := database.Connection()
 	userRepository := repositories.NewUserRespository(db)
 	roleRepository := repositories.NewRoleRepository(db)
@@ -37,27 +55,30 @@ func CreateUser(ctx *fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusBadRequest, "Usuario ya existe.")
 	}
 
+	var filenameImage string
+	if imageIsDefinedAndValid {
+		filename := uuid.NewString()
+		filenameImage, err = helpers.SaveImageToDisk(image, filename, imageType, models.PATH_PROFILE_IMAGES)
+
+		if err != nil {
+			return fiber.NewError(fiber.StatusInternalServerError, "No se pudo guardar la imagen del usuario")
+		}
+
+	}
+
 	err, newUser := userRepository.CreateUser(
 		request.Name,
 		request.Email,
 		request.RoleId,
 		request.Password,
 		request.Active,
+		filenameImage,
+		request.EmailVerified,
 	)
 
 	if err != nil {
 		return fiber.NewError(fiber.StatusInternalServerError, "No se pudo crear el usuario")
 	}
 
-	if request.EmailVerified {
-		if err = userRepository.SetEmailVerified(newUser.ID); err != nil {
-			fiber.NewError(fiber.StatusInternalServerError, "No se pudo setear la fecha de actualizacion en verificación del correo del usuario.")
-		}
-	}
-
-	newUser = userRepository.GetUserById(newUser.ID)
-
-	return ctx.JSON(fiber.Map{
-		"usuario": newUser,
-	})
+	return ctx.Status(fiber.StatusCreated).JSON(entities.CreateUserResponse(&newUser))
 }
