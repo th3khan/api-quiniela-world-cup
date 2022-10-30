0c69d4d86604f29a393943e979b6b949713232b9
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-10-30 11:20:39 -0400

    create entity base, create and update requests

--- pkg/entities/admin/UserEntities.go
+++ pkg/entities/admin/UserEntities.go
@@ -8,14 +8,24 @@ type (
 		entities.Pagination
 	}
 
-	CreateUserRequest struct {
-		Name                 string `json:"name" validate:"required"`
-		Email                string `json:"email" validate:"required,email"`
+	UserBaseRequest struct {
+		Name          string `json:"name" validate:"required"`
+		Email         string `json:"email" validate:"required,email"`
+		Active        bool   `json:"active" validate:"required"`
+		RoleId        uint   `json:"role_id" validate:"required"`
+		EmailVerified bool   `json:"email_verified" validate:"required"`
+		Image         string `json:"image"`
+	}
+
+	UserCreateRequest struct {
+		UserBaseRequest
 		Password             string `json:"password" validate:"required"`
 		PasswordConfirmation string `json:"password_confirmation" validate:"required,eqfield=Password"`
-		Active               bool   `json:"active" validate:"required"`
-		RoleId               uint   `json:"role_id" validate:"required"`
-		EmailVerified        bool   `json:"email_verified" validate:"required"`
-		Image                string `json:"image"`
+	}
+
+	UserUpdateRequest struct {
+		UserBaseRequest
+		Password             string `json:"password"`
+		PasswordConfirmation string `json:"password_confirmation" validate:"eqfield=Password"`
 	}
 )
