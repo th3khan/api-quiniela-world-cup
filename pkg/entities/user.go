467b8df2103b75a694678c3ebfb1e65fb4b38dc1
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-11-14 13:51:20 -0400

    add email verified to user response

--- pkg/entities/user.go
+++ pkg/entities/user.go
@@ -4,26 +4,28 @@ import "github.com/th3khan/api-quiniela-world-cup/app/models"
 
 type (
 	UserResponse struct {
-		ID     uint         `json:"id"`
-		Name   string       `json:"name"`
-		Email  string       `json:"email"`
-		RoleId uint         `json:"role_id"`
-		Role   RoleResponse `json:"role"`
-		Image  string       `json:"image"`
-		Points float32      `json:"points"`
-		Active bool         `json:"active"`
+		ID            uint         `json:"id"`
+		Name          string       `json:"name"`
+		Email         string       `json:"email"`
+		RoleId        uint         `json:"role_id"`
+		Role          RoleResponse `json:"role"`
+		Image         string       `json:"image"`
+		Points        float32      `json:"points"`
+		Active        bool         `json:"active"`
+		EmailVerified bool         `json:"email_verified"`
 	}
 )
 
 func CreateUserResponse(userModel *models.User) UserResponse {
 	return UserResponse{
-		ID:     userModel.ID,
-		Name:   userModel.Name,
-		Email:  userModel.Email,
-		RoleId: userModel.RoleId,
-		Role:   CreateRoleResponse(&userModel.Role),
-		Image:  userModel.Image,
-		Points: userModel.Points,
-		Active: userModel.Active,
+		ID:            userModel.ID,
+		Name:          userModel.Name,
+		Email:         userModel.Email,
+		RoleId:        userModel.RoleId,
+		Role:          CreateRoleResponse(&userModel.Role),
+		Image:         userModel.Image,
+		Points:        userModel.Points,
+		Active:        userModel.Active,
+		EmailVerified: userModel.EmailVerified,
 	}
 }
